81557bcf5739ef911c9cb9d2a3b79622e3f9323c
Eliseika <57280038+Eliseika@users.noreply.github.com>
2020-12-08 13:41:51 +0500

    Update lab5-edit_exam.php

--- lab5-edit_exam.php
+++ lab5-edit_exam.php
@@ -1,8 +1,12 @@
-﻿<?php
+<?php
 require_once 'lab5-connect.php';
 $id=$_GET['exam_ID'];
-$exams = mysqli_query($connect, "SELECT * FROM `Exam_schedule` WHERE `exam_ID`= '$id' ");
+$exams = mysqli_query($connect, "SELECT Exam_schedule.exam_ID as exam_ID, Exam_schedule.exam_date as exam_date, Exam_schedule.exam_consultation_date as exam_consultation_date, Exam_schedule.group_ID as group_ID, Exam_schedule.predm_ID as predm_ID, Exam_schedule.exam_audience as exam_audience, Subjects.predm_name as predm_name,  Group.group_name as group_name FROM `Exam_schedule` LEFT JOIN `Subjects` ON Exam_schedule.predm_ID=Subjects.predm_ID LEFT JOIN `Group` ON Exam_schedule.group_ID=Group.group_ID WHERE `exam_ID`= '$id' ");
 $exam= mysqli_fetch_assoc($exams);
+$group_ID = $exam['group_ID'];
+$group_name = $exam['group_name'];
+$predm_ID = $exam['predm_ID'];
+$predm_name = $exam['predm_name'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,8 +37,32 @@ background:#b5ecdc;
 <th>Аудитория</th>
 </tr>
 <tr>
-<td><input type="number" name="group_ID" value="<?= $exam['group_ID']?>"></td>
-<td><input type="number" name="predm_ID" value="<?= $exam['predm_ID']?>"></td>
+<td><?php
+$result = mysqli_query($connect,"SELECT * FROM `Group`");
+echo "<select name='group_ID'>";
+echo "<option value='$group_ID'>$group_name</option>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$group_ID = $row['group_ID'];
+$group_name = $row['group_name'];
+echo "<option value='$group_ID'>$group_name</option>";
+}
+}
+echo "</select>";
+?></td>
+<td><?php
+$result = mysqli_query($connect, "SELECT * FROM `Subjects`");
+echo "<select name='predm_ID'>";
+echo "<option value='$predm_ID'>$predm_name</option>";
+if ($result) {
+while ($row = $result->fetch_array()) {
+$predm_ID = $row['predm_ID'];
+$predm_name = $row['predm_name'];
+echo "<option value='$predm_ID'>$predm_name</option>";
+}
+}
+echo "</select>";
+?></td>
 <td><input type="text" name="exam_consultation_date" value="<?= $exam['exam_consultation_date']?>"></td>
 <td><input type="text" name="exam_date" value="<?= $exam['exam_date']?>"></td>
 <td><input type="text" name="exam_audience" value="<?= $exam['exam_audience']?>" ></td>
